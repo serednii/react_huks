d54625b1574cfeb8b7d74a595dfe8500ce9db4d5
Serednii Mykola <seredniimykola@gmail.com>
2022-10-30 06:58:45 +0100

    first commit photos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Collection } from './Collection'
 import './index.scss';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
-
+// https://635d74d2ea764497f0dd237e.mockapi.io/Collections
+// https://635d74d2ea764497f0dd237e.mockapi.io/Collections
 function App() {
+
+  const temp = [];
+
+  const [collections, setCollections] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://635d74d2ea764497f0dd237e.mockapi.io/Collections')
+      .then((res) => res.json)
+      .then((json) => {
+        temp = json;
+        // setCollections(json);
+        console.log(json)
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении даних');
+      })
+  });
+
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
@@ -30,15 +39,11 @@ function App() {
         <input className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {/* {
+          collections.map((obj) => (
+            <Collection name={obj.name} images={obj.images} />
+          ))
+        } */}
       </div>
       <ul className="pagination">
         <li>1</li>
