32e24d885cdb72d6e4a9c47e4f8dbf4e3edda873
Serednii Mykola <seredniimykola@gmail.com>
2022-11-04 19:57:36 +0100

    first commit

--- src/List.jsx
+++ src/List.jsx
@@ -1,98 +1,60 @@
 import React from "react";
 
-// class List extends React.Component {
-//     state = {
-//         number: [1, 2, 3],
-//     };
-
-// addRandomNumber = () => {
-//     const randNumber = Math.round(Math.random() * 100);
-//     this.setState({
-//         number: [...this.state.number, randNumber],
-//     });
-// };
-
-// componentDidMount() {
-//     console.log('Компонент бил отображон')
-// }
-
-// componentDidUpdate(prevProps, prevState) {
-//     console.log(prevProps)
-//     console.log(prevState)
-//     console.log(this.props)
-//     console.log(this.state)
-// }
-
-// componentWillUnmount() {
-//     console.log('Component wiil by deleted')
-// }
-
-//     render() {
-//         return (
-//             <div>
-//                 <ul>
-//                     {
-//                         this.state.number.map((n, i) => (
-//                             <li key={i}>{n}</li>
-//                         ))
-//                     }
-//                 </ul>
-//                 <button onClick={this.addRandomNumber}>Новое число</button>
-//             </div >
-//         )
-//     }
-// }
-
-
 const List = () => {
-    const [number, setNumber] = React.useState([1, 2, 3, 5, 7, 4, 5, 6, 7, 8, 9]);
-    const [filterNumber, setFilterNumber] = React.useState();
-    const [count, setCount] = React.useState(0);
+    const [number, setNumber] = React.useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    let ulRef = React.useRef();
 
-
-    const numberAdd = () => {
-        const n = Math.round((Math.random() * 1000));
-        setNumber([...number, n]);
+    const addNumber = () => {
+        const lastNumber = number[number.length - 1];
+        setNumber([...number, lastNumber + 1]);
+        setNumber((prev) => [...prev, prev[prev.length - 1] + 1]);
     }
 
-    const filter = (value) => {
-        // debugger;
-        setFilterNumber(+value)
+    const handleScroll = () => {
+        console.log('will by scroll');
     }
+    let timer;
 
     React.useEffect(() => {
-        console.log('the component count  was update');
-    }, [count]);
 
-    React.useEffect(() => {
-        console.log('the component number  was update');
-    }, [number]);
-    React.useEffect(() => {
-        console.log('the component   was update');
-    });
-
-    React.useEffect(() => {
-        console.log('the component was displayed');
-        return () => {
-            console.log('component deleted')
-        }
+        console.log(ulRef);
+        ulRef.current.addEventListener('click', handleScroll);
+        // ulRef = document.querySelector('ul');
+        // ulRef.addEventListener('click', handleScroll);
     }, []);
+
+    React.useEffect(() => {
+        console.log(ulRef);
+    }, [number]);
+
+    const removeScroll = () => {
+        ulRef.current.removeEventListener('click', handleScroll);
+    }
+
+    const start = () => {
+        timer = setInterval(addNumber, 1000);
+    }
+
+    const stop = () => {
+        clearInterval(timer);
+    }
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={numberAdd}>Новое число</button>
-            <ul>
+            <button onClick={addNumber}>Новое число</button>
+            <button onClick={removeScroll}>Не следить</button>
+            <button onClick={start}>Старт</button>
+            <button onClick={stop}>Стоп</button>
+
+            <ul ref={ulRef} className="list">
                 {
-                    number.filter((n) => filterNumber ? (n === filterNumber) : (true))
-                        .map((n, i) => (
-                            <li key={i}>{n}</li>
-                        ))
+                    number.map((n, i) => (
+                        <li key={i}>{n}</li>
+                    ))
                 }
             </ul>
 
-            <textarea onChange={(e) => filter(e.target.value)}></textarea>
+
         </div>
 
     )
