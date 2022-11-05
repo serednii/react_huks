d52456664622998ca043e8f1e3fd3cef31beb321
Serednii Mykola <seredniimykola@gmail.com>
2022-11-05 18:46:48 +0100

    save change

--- src/List.jsx
+++ src/List.jsx
@@ -1,42 +1,37 @@
 import React from "react";
 
 const List = () => {
-    const [number, setNumber] = React.useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
-    let ulRef = React.useRef();
+    const [number, setNumber] = React.useState([1, 2, 3, 4]);
+    let timerRef = React.useRef();
+    let timer = React.useRef();
+
 
     const addNumber = () => {
-        const lastNumber = number[number.length - 1];
-        setNumber([...number, lastNumber + 1]);
         setNumber((prev) => [...prev, prev[prev.length - 1] + 1]);
     }
 
     const handleScroll = () => {
-        console.log('will by scroll');
+        console.log();
     }
-    let timer;
 
     React.useEffect(() => {
-
-        console.log(ulRef);
-        ulRef.current.addEventListener('click', handleScroll);
-        // ulRef = document.querySelector('ul');
-        // ulRef.addEventListener('click', handleScroll);
-    }, []);
-
-    React.useEffect(() => {
-        console.log(ulRef);
+        console.log(number);
     }, [number]);
 
+    React.useEffect(() => {
+        timerRef.current.addEventListener('click', handleScroll);
+    }, []);
+
     const removeScroll = () => {
-        ulRef.current.removeEventListener('click', handleScroll);
+        timerRef.current.removeEventListener('click', handleScroll);
     }
 
     const start = () => {
-        timer = setInterval(addNumber, 1000);
+        timer.current = setInterval(addNumber, 1000);
     }
 
     const stop = () => {
-        clearInterval(timer);
+        clearInterval(timer.current);
     }
 
     return (
@@ -46,7 +41,7 @@ const List = () => {
             <button onClick={start}>Старт</button>
             <button onClick={stop}>Стоп</button>
 
-            <ul ref={ulRef} className="list">
+            <ul ref={timerRef} className="list">
                 {
                     number.map((n, i) => (
                         <li key={i}>{n}</li>
