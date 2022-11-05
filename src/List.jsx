fa66dc5c8b48475294b9dc1737c3947de087512e
Serednii Mykola <seredniimykola@gmail.com>
2022-11-05 19:42:39 +0100

    useCallback

--- src/List.jsx
+++ src/List.jsx
@@ -1,47 +1,49 @@
 import React from "react";
 
 const List = () => {
-    const [number, setNumber] = React.useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    const [number, setNumber] = React.useState([1, 2, 3]);
     let ulRef = React.useRef();
+    let timer = React.useRef();
+    let numberUseref = React.useRef();
+    numberUseref.current = number;
 
     const addNumber = () => {
-        const lastNumber = number[number.length - 1];
-        setNumber([...number, lastNumber + 1]);
         setNumber((prev) => [...prev, prev[prev.length - 1] + 1]);
     }
 
-    const handleScroll = () => {
-        console.log('will by scroll');
-    }
-    let timer;
+    const handleScroll = React.useCallback(() => {
+        console.log('will by scroll', numberUseref.current);
+    }, []);
 
     React.useEffect(() => {
-
         console.log(ulRef);
         ulRef.current.addEventListener('click', handleScroll);
-        // ulRef = document.querySelector('ul');
-        // ulRef.addEventListener('click', handleScroll);
     }, []);
 
     React.useEffect(() => {
         console.log(ulRef);
     }, [number]);
 
+    const addScroll = () => {
+        ulRef.current.addEventListener('click', handleScroll);
+    }
+
     const removeScroll = () => {
         ulRef.current.removeEventListener('click', handleScroll);
     }
 
     const start = () => {
-        timer = setInterval(addNumber, 1000);
+        timer.current = setInterval(addNumber, 1000);
     }
 
     const stop = () => {
-        clearInterval(timer);
+        clearInterval(timer.current);
     }
 
     return (
         <div>
             <button onClick={addNumber}>Новое число</button>
+            <button onClick={addScroll}>Cледить</button>
             <button onClick={removeScroll}>Не следить</button>
             <button onClick={start}>Старт</button>
             <button onClick={stop}>Стоп</button>
